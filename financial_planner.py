715cb43ddf91bc19eac3112c0597d85b0d79f10e
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2025-03-19 06:35:51 +0200

    Add files via upload
    financtial planner file first commit

---
+++ financial_planner.py
@@ -0,0 +1,38 @@
+import json
+from datetime import datetime
+current_date = datetime.now().strftime("%m-%d-%y")
+
+# Placeholder for AI integration (An API or local model)
+def ai_guidance(goal,finces,swot):
+    pass
+
+# class to manage financial data and goals
+class FinancialPlanner:
+    def __init__(self):
+        pass
+
+    def input_finances(self):
+        pass
+
+    def input_goals(self):
+        pass
+
+    def input_swot(self):
+        pass
+
+    def calculate_progress(self):
+        pass
+
+    def save_data(self, filename="fincaial_plan.json"):
+        pass
+
+    def load_data(self, filename="financial_plan.json"):
+        pass
+
+# Main program
+def main():
+    planner = FinancialPlanner()
+
+if __name__ == "__main__":
+    print(f"Financial Planner - Today's Date: {current_date}")
+    main()
