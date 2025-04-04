b8f1c122f88ac203e8f564c2c3e448eed6872425
kamtimacjc025 <kamtimacjc025@student.wethinkcode.co.za>
2025-04-04 15:16:13 +0200

    passing a few tests

--- financial_planner.py
+++ financial_planner.py
@@ -13,7 +13,33 @@ class FinancialPlanner:
 
     def input_finances(self):
         pass
-
+    def get_long_goal(self):
+        while True:
+            long_goal = input("Insert your long goal\n").lower()
+            if long_goal.isalpha():
+                break
+            print("Invalid long goal")
+            
+        print(f"Your goal is {long_goal}")
+        return long_goal
+    def get_medium_goal(self):
+        while True:
+            medium_goal = input("Insert your medium goal\n").lower()
+            if medium_goal.isalpha():
+                break
+            print("Invalid medium goal")
+            
+        print(f"Your goal is {medium_goal}")
+        return medium_goal
+    def get_short_goal(self):
+        while True:
+            short_goal = input("Insert your short goal\n").lower()
+            if short_goal.isalpha():
+                break
+            print("Invalid short goal")
+            
+        print(f"Your goal is {short_goal}")
+        return short_goal
     def input_goals(self):
         pass
 
