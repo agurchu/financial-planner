18bb39b1fbedb1a156b7836321664e9fcb1e039d
Katlego J. Mtimane <katlegoj.dev@gmail.com>
2025-04-07 12:15:06 +0200

    input finances test passes

--- financial_planner.py
+++ financial_planner.py
@@ -12,7 +12,48 @@ class FinancialPlanner:
         pass
 
     def input_finances(self):
-        pass
+        """
+            This funtion collects user's income and expenses and store them into a dictionary for later use
+        """
+        
+        finances = {
+            "Income": 0,
+            "Expenses": {}
+        }
+        expenses = ""
+        while True:
+            montly_income = input("Insert your total monthly income").strip()
+            if montly_income.isdigit():
+                finances["Income"] = int(montly_income)
+                break
+            print("Invalid income prompt")
+            
+        expenses_run = True
+        while expenses_run:
+            while True:
+                expense_name = input("Insert your expense name(e.g. rent, food) or done if you are done").strip().lower()
+                if expense_name == "done":
+                    expenses_run = False
+                    break
+                elif expense_name.isalpha():
+                    finances["Expenses"][expense_name] = 0
+                    break
+                print("Invalid expense name")
+            while expenses_run:
+                expense_amount = input("Insert its amount(e.g. 1500)").strip()
+                if expense_amount.isdigit():
+                    finances["Expenses"][expense_name] = int(expense_amount)
+                    break
+                print("Invalid expense amount")
+        for expense, amount in finances["Expenses"].items():
+            expenses += f"{expense}: R{str(amount)},"
+        income = finances.get("Income")
+        expense_clean = expenses.strip(",")
+        print(f"\nIncome: (R{income})")
+        # print(f"Expenses: ({",".join(key+": R"+value for key,value in finances["Expenses"].items())})")
+        print(f"Expenses: ({expense_clean})")
+    
+    # getting long goal from user
     def get_long_goal(self):
         while True:
             long_goal = input("Insert your long goal\n").lower()
@@ -22,6 +63,8 @@ class FinancialPlanner:
             
         print(f"Your goal is {long_goal}")
         return long_goal
+    
+    # getting medium goal from user
     def get_medium_goal(self):
         while True:
             medium_goal = input("Insert your medium goal\n").lower()
@@ -31,6 +74,8 @@ class FinancialPlanner:
             
         print(f"Your goal is {medium_goal}")
         return medium_goal
+    
+    # getting short goal from user
     def get_short_goal(self):
         while True:
             short_goal = input("Insert your short goal\n").lower()
