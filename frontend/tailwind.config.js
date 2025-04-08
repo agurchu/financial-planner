ca7506c6b658c131f966a9e900c5a7fccb5d5ae0
Katlego J. Mtimane <katlegoj.dev@gmail.com>
2025-04-08 15:59:26 +0200

    frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,jsx,ts,tsx}", // or adjust this path to match where your source files are
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
