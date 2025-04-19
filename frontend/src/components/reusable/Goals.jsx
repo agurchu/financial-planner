652aacaf2781da430461d335da7885f91ee46def
kamtimacjc025 <kamtimacjc025@student.wethinkcode.co.za>
2025-04-19 14:36:18 +0200

    goal progress

--- frontend/src/components/reusable/Goals.jsx
+++ frontend/src/components/reusable/Goals.jsx
@@ -3,8 +3,10 @@ import Goal from "./Goal.jsx"
 
 export default function Goals() {
   return (
-    <div>Goals
-        <Goal />
+    <div className='grid gap-2'>
+        <Goal name="Vacation Fund" current_cash={15000} target_cash={20000}  />
+        <Goal name="New Car" current_cash={150000} target_cash={250000}  />
+        <Goal name="New Phone" current_cash={1200} target_cash={8000}  />
     </div>
   )
 }
